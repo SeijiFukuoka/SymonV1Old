5fccce891a4dd146c50a7007aadfe56ef72d363b
Seiji Fukuoka <seijifukuoka@gmail.com>
2017-11-09 23:00:58 -0200

    Update login confirmation layout

--- app/src/main/kotlin/br/com/symon/ui/login/LoginActivity.kt
+++ app/src/main/kotlin/br/com/symon/ui/login/LoginActivity.kt
@@ -3,6 +3,10 @@ package br.com.symon.ui.login
 import android.os.Bundle
 import br.com.symon.R
 import br.com.symon.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_login.buttonLogin
+import kotlinx.android.synthetic.main.activity_login.editTextLoginEmail
+import kotlinx.android.synthetic.main.layout_toolbar.imageBackArrow
+import kotlinx.android.synthetic.main.layout_toolbar.toolbar
 
 class LoginActivity : BaseActivity() {
 
@@ -10,6 +14,18 @@ class LoginActivity : BaseActivity() {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_login)
 
-    showLoading()
+    setSupportActionBar(toolbar)
+    supportActionBar?.setDisplayShowHomeEnabled(false)
+    supportActionBar?.setDisplayShowTitleEnabled(false)
+
+    imageBackArrow.setOnClickListener {
+      onBackPressed()
+    }
+
+    buttonLogin.setOnClickListener({
+      val recipeDetailActivity = LoginConfirmationActivity.newIntent(this,
+          editTextLoginEmail.text.toString())
+      startActivity(recipeDetailActivity)
+    })
   }
 }
---
+++ app/src/main/kotlin/br/com/symon/ui/login/LoginConfirmationActivity.kt
@@ -0,0 +1,46 @@
+package br.com.symon.ui.login
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import br.com.symon.R.layout
+import br.com.symon.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_login_confirmation.editTextLoginEmail
+import kotlinx.android.synthetic.main.activity_login_confirmation.editTextPassword
+import kotlinx.android.synthetic.main.layout_toolbar.imageBackArrow
+import kotlinx.android.synthetic.main.layout_toolbar.toolbar
+
+class LoginConfirmationActivity : BaseActivity() {
+
+  companion object {
+
+    private val INTENT_EMAIL_EXTRA = "email_extra"
+
+    lateinit var email: String
+
+    fun newIntent(context: Context, email: String?): Intent {
+      val intent = Intent(context, LoginConfirmationActivity::class.java)
+      intent.putExtra(INTENT_EMAIL_EXTRA, email)
+      return intent
+    }
+  }
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(layout.activity_login_confirmation)
+
+    setSupportActionBar(toolbar)
+    supportActionBar?.setDisplayShowHomeEnabled(false)
+    supportActionBar?.setDisplayShowTitleEnabled(false)
+
+    imageBackArrow.setOnClickListener {
+      onBackPressed()
+    }
+
+    email = intent.extras.getString(INTENT_EMAIL_EXTRA)
+    editTextLoginEmail.setText(email)
+
+
+    editTextPassword.requestFocus()
+  }
+}
