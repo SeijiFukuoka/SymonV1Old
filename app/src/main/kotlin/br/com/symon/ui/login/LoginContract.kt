c6a440a22c1d5dc09a5bc94d3c6bb477bd528ecc
Seiji Fukuoka <seijifukuoka@gmail.com>
2017-11-10 21:30:16 -0200

    Add login dependency injection (wip)

---
+++ app/src/main/kotlin/br/com/symon/ui/login/LoginContract.kt
@@ -0,0 +1,14 @@
+package br.com.symon.ui.login
+
+import br.com.symon.base.BaseView
+import br.com.symon.data.model.User
+
+class LoginContract {
+  interface View : BaseView {
+    fun showLoginResponse(user: User?)
+  }
+
+  interface Presenter {
+    fun login(user: User?)
+  }
+}
