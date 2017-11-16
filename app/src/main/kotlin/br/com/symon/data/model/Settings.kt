35c0bc4361e49a75fba791ac4de6eaae5d99901b
Seiji Fukuoka <seijifukuoka@gmail.com>
2017-11-15 23:40:16 -0200

    [FEAT] Add settings and notification models

--- app/src/main/kotlin/br/com/symon/data/model/Settings.kt
+++ app/src/main/kotlin/br/com/symon/data/model/Settings.kt
@@ -1,5 +1,24 @@
 package br.com.symon.data.model
 
+import android.os.Parcel
+import android.os.Parcelable
 
-class Settings {
+data class Settings(var settingsList: MutableList<NotificationStatus>?) : Parcelable {
+    constructor(source: Parcel) : this(
+            source.createTypedArrayList(NotificationStatus.CREATOR)
+    )
+
+    override fun describeContents() = 0
+
+    override fun writeToParcel(dest: Parcel, flags: Int) = with(dest) {
+        writeTypedList(settingsList)
+    }
+
+    companion object {
+        @JvmField
+        val CREATOR: Parcelable.Creator<Settings> = object : Parcelable.Creator<Settings> {
+            override fun createFromParcel(source: Parcel): Settings = Settings(source)
+            override fun newArray(size: Int): Array<Settings?> = arrayOfNulls(size)
+        }
+    }
 }
